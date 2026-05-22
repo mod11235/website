44f658058cde624d3cf25a764805053503e979e8
elee <laniel2@illinois.edu>
2026-05-22 02:13:55 -0500

    fixes

---
+++ js/layout.js
@@ -0,0 +1,7 @@
+<script>
+    function toggleSidebar() {
+        document.querySelector(".sidebar").classList.toggle("active");
+        document.querySelector(".overlay").classList.toggle("active");
+        document.querySelector(".main").classList.toggle("shift");
+    }
+</script>
