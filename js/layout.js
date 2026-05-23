feb600f718df424f408c8d64d56ca4413ccaa504
elee <laniel2@illinois.edu>
2026-05-22 19:49:17 -0500

    layout removal

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,7 @@
+/*
 function toggleSidebar() {
     document.querySelector(".sidebar").classList.toggle("active");
     document.querySelector(".overlay").classList.toggle("active");
     document.querySelector(".main").classList.toggle("shift");
 }
+    */
